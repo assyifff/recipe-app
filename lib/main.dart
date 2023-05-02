5993f10dc962c1bde0c1adcc53589748e77dea1a
assyifff <syifanurulfitri@outlook.com>
2023-05-02 15:43:10 +0700

    update recipe database

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/core/helper/recipe_helper.dart';
 import 'package:recipe_app/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await RecipeHelper.recipeHelper.initDatabase();
   runApp(const MyApp());
 }
